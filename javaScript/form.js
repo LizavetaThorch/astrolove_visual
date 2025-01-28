ab756526f2d37973bc99c84d3f508001f1b5b89d
Greta135 <garshina.rita@yandex.ru>
2025-01-28 14:19:27 +0300

    new pages, form, numbers on pages

---
+++ javaScript/form.js
@@ -0,0 +1,30 @@
+// Заполнение списка с днями (от 1 до 31)
+const daysSelect = document.getElementById("day");
+for (let i = 1; i <= 31; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    daysSelect.appendChild(option);
+}
+
+// Заполнение списка с месяцами
+const monthsSelect = document.getElementById("month");
+const months = [
+    "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", 
+    "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+];
+months.forEach((month, index) => {
+    const option = document.createElement("option");
+    option.value = index + 1;
+    option.textContent = month;
+    monthsSelect.appendChild(option);
+});
+
+// Заполнение списка с годами (от 2024 до 1920)
+const yearsSelect = document.getElementById("year");
+for (let year = 2024; year >= 1924; year--) {
+    const option = document.createElement("option");
+    option.value = year;
+    option.textContent = year;
+    yearsSelect.appendChild(option);
+}
