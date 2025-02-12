f2c4fdd85017ae949a73db2fac0e491292cb5f77
Greta135 <garshina.rita@yandex.ru>
2025-02-13 02:09:17 +0300

    full registration in js without pages

--- javaScript/mainPage.js
+++ javaScript/mainPage.js
@@ -1,33 +1,102 @@
-// Заполнение списка с днями (от 1 до 31)
-const daysSelect = document.getElementById("day");
-for (let i = 1; i <= 31; i++) {
-    const option = document.createElement("option");
-    option.value = i;
-    option.textContent = i;
-    daysSelect.appendChild(option);
-}
 
-// Заполнение списка с месяцами
-const monthsSelect = document.getElementById("month");
-const months = [
-    "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
-    "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+// Заполнение списка с разницей в возрасте старше
+const gapagesSelect = document.getElementById("gapage");
+const gapages = [
+    "Категорическое нет", "1-3 года", "4-7 лет", "8-11 лет", "12 и больше", "Не имеет значения"
 ];
-months.forEach((month, index) => {
+gapages.forEach((gapage, index) => {
     const option = document.createElement("option");
     option.value = index + 1;
-    option.textContent = month;
-    monthsSelect.appendChild(option);
+    option.textContent = gapage;
+    gapagesSelect.appendChild(option);
 });
 
-// Заполнение списка с годами (от 2024 до 1924)
-const yearsSelect = document.getElementById("year");
-for (let year = 2024; year >= 1924; year--) {
+// Заполнение списка с разницей в возрасте младше
+const agegapsSelect = document.getElementById("agegap");
+const agegaps = [
+    "Категорическое нет", "1-3 года", "4-7 лет", "8-11 лет", "12 и больше", "Не имеет значения"
+];
+agegaps.forEach((agegap, index) => {
     const option = document.createElement("option");
-    option.value = year;
-    option.textContent = year;
-    yearsSelect.appendChild(option);
-}
+    option.value = index + 1;
+    option.textContent = agegap;
+    agegapsSelect.appendChild(option);
+});
+// Заполнение списка со статусом отношений
+const statussSelect = document.getElementById("status");
+const statuss = [
+    "В активном поиске", "Свободна/Свободен", "В отношениях", "Замужем/Женат", "Развод/В разводе"
+];
+statuss.forEach((status, index) => {
+    const option = document.createElement("option");
+    option.value = index + 1;
+    option.textContent = status;
+    statussSelect.appendChild(option);
+});
+// Заполнение списка про детей
+const childrensSelect = document.getElementById("children");
+const childrens = [
+    "Дети имеются, хорошо к ним отношусь", "Дети есть, больше не хочу детей", "Детей нет, хорошо к ним отношусь", "Детей нет, не хочу детей"
+];
+childrens.forEach((children, index) => {
+    const option = document.createElement("option");
+    option.value = index + 1;
+    option.textContent = children;
+    childrensSelect.appendChild(option);
+});
+// Заполнение списка про отношения на расстоянии
+const distancesSelect = document.getElementById("distance");
+const distances = [
+    "Да", "Нет ", "Не уверен(а)"
+];
+distances.forEach((distance, index) => {
+    const option = document.createElement("option");
+    option.value = index + 1;
+    option.textContent = distance;
+    distancesSelect.appendChild(option);
+});
+
+// Заполнение списка с ориентацией
+const orientationsSelect = document.getElementById("orientation");
+const orientations = [
+    "Гетеросексуал", "Бисексуал", "Гей", "Лесбиянка"
+];
+orientations.forEach((orientation, index) => {
+    const option = document.createElement("option");
+    option.value = index + 1;
+    option.textContent = orientation;
+    orientationsSelect.appendChild(option);
+});
+// Заполнение списка с целью поисков
+const searchingsSelect = document.getElementById("searching");
+const searchings = [
+    "Найти друга (дружеские отношения)", "Найти подругу (дружеские отношения)", "Найти бизнес-партнера//коллегу ", "Найти врага","Найти человека для путешествий", "Найти любовь всей жизни", "Построить отношения ", "Построить серьезные отношения","Найти мужа", "Найти жену", "Найти парня ", "Найти девушку","Подыскать полового партнера без обязательств (вам должно быть больше 18+)"
+];
+searchings.forEach((searching, index) => {
+    const option = document.createElement("option");
+    option.value = index + 1;
+    option.textContent = searching;
+    searchingsSelect.appendChild(option);
+});
+// Первая кнопка далее с открыванием анкеты
+document.getElementById("next").addEventListener("click", 
+    function regist() {
+    document.getElementById("regist1").style.display="none";
+    document.getElementById("regist2").style.display="block";
+    document.getElementById("next").style.display="none";
+    document.getElementById("next1").style.display="block";
+    document.getElementById("rectangle2").style.display="block";
+    
+});
+  // Вторая кнопка далее с открыванием оплаты
+document.getElementById("next1").addEventListener("click", 
+    function reg() {
+    document.getElementById("regist2").style.display="none";
+    document.getElementById("regist3").style.display="block";
+    document.getElementById("rectangle3").style.display="block";
+    
+});
+  
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("save").addEventListener("click", (event) => {
