7a81947beb302a1e513764c31621f556f1c22676
Greta135 <garshina.rita@yandex.ru>
2025-02-19 23:35:30 +0300

    new image match, new menu profile

--- javaScript/match.js
+++ javaScript/match.js
@@ -10,14 +10,26 @@ if (!userId) {
 console.log("user_id:", userId); // Проверка user_id
 
 
-//если жмет да, тут даем ссылку на профиль второго юзера и заносим данные в таблицу мэтчей, что этот юзер ответил да
+//это про мэтчи. если жмет да, тут даем ссылку на профиль второго юзера и заносим данные в таблицу мэтчей, что этот юзер ответил да
   function confirmYes() {
         document.getElementById("username").hidden=false
       document.getElementById("confirm").hidden=true
   }
-   //если жмет нет, передаем данные в таблицу и окно уходит у обоих юзеров
+   //если жмет нет, передаем данные в таблицу и окно уходит у обоих юзеров-как это?
   //если жмет нет, скрываем мэтч (удаляем его?)
   function confirmNo() {
     document.getElementById("confirm").hidden=true
       document.getElementById("continue").hidden=false
 };
+
+// Заполнение списка с целью поисков
+const searchingsSelect = document.getElementById("searching");
+const searchings = [
+    "Найти друга (дружеские отношения)//Найти подругу (дружеские отношения)", "Найти бизнес-партнера//коллегу ", "Найти человека для путешествий", "Построить отношения ", "Найти мужа//Найти жену", "Найти парня//Найти девушку", "Подыскать полового партнера без обязательств (вам должно быть больше 18+)"
+];
+searchings.forEach((searching, index) => {
+    const option = document.createElement("option");
+    option.value = index + 1;
+    option.textContent = searching;
+    searchingsSelect.appendChild(option);
+});
