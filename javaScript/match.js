2475577c941442b72f0b0f04d0dd3e6889eb5d63
Greta135 <garshina.rita@yandex.ru>
2025-03-03 12:15:42 +0300

    button for redacting profile

--- javaScript/match.js
+++ javaScript/match.js
@@ -16,11 +16,13 @@ document.getElementById("payment").addEventListener("click",
     function tarif() {
         document.getElementById("prof1").style.display = "none";
         document.getElementById("prof2").style.display = "block";
+    document.getElementById("changeprof").style.display = "none";
     });
  document.getElementById("profile").addEventListener("click",
     function prof() {
         document.getElementById("prof2").style.display = "none";
         document.getElementById("prof1").style.display = "block";
+     document.getElementById("changeprof").style.display = "none";
     });
 //это про мэтчи. если жмет да, тут даем ссылку на профиль второго юзера и заносим данные в таблицу мэтчей, что этот юзер ответил да
   function confirmYes() {
@@ -33,7 +35,12 @@ document.getElementById("payment").addEventListener("click",
     document.getElementById("confirm").hidden=true
       document.getElementById("continue").hidden=false
 };
-
+// Кнопка на редактирование анкеты
+document.getElementById("change").addEventListener("click",
+    function redact() {
+        document.getElementById("prof1").style.display = "none";
+        document.getElementById("changeprof").style.display = "block";
+    });
 // Заполнение списка с целью поисков для редактирования анкеты
 const searchingsSelect = document.getElementById("searching");
 const searchings = [
@@ -44,4 +51,36 @@ searchings.forEach((searching, index) => {
     option.value = index + 1;
     option.textContent = searching;
     searchingsSelect.appendChild(option);
+});
+
+// Заполнение списка с целью поисков для редактирования анкеты
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("save").addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("Кнопка Save нажата!");
+
+        const searching = document.getElementById("searching").value;
+        
+         if (!searching ) {
+             alert("Заполните все поля!");
+             return;
+         }
+        
+        userData = {
+            searching: searching,
+        };
+        
+        console.log("Отправляем данные в бота: ", JSON.stringify(userData));
+
+        if (window.Telegram && window.Telegram.WebApp) {
+            Telegram.WebApp.ready();
+            console.log("Telegram WebApp инициализирован!");
+
+            Telegram.WebApp.sendData(JSON.stringify(userData));
+            console.log("Данные отправлены в бота!");
+        } else {
+            console.error("Telegram WebApp API не доступен!");
+            alert("Telegram WebApp API не доступен!");
+        }
+         });
 });
